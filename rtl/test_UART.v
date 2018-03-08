19a0001772ccf2a40c0fa2ff6d46239b2482ba91
Phung Cheng Fei <feiphung@hotmail.com>
2018-03-08 08:19:40 +0800

    Solved induction bugs

--- rtl/test_UART.v
+++ rtl/test_UART.v
@@ -169,7 +169,8 @@ begin
 			else if((cnt > 0) && (cnt <= INPUT_DATA_WIDTH)) begin  // during UART data transmission
 				
 				if(state >= NUMBER_OF_RX_SYNCHRONIZERS) begin
-					assert((state-NUMBER_OF_RX_SYNCHRONIZERS) < Rx_STOP_BIT);
+					assert((state-NUMBER_OF_RX_SYNCHRONIZERS) <= Rx_DATA_BIT_7);
+					assert((state-NUMBER_OF_RX_SYNCHRONIZERS) >= Rx_DATA_BIT_0);
 				end
 				
 				assert(data_is_valid == 0);
@@ -199,6 +200,7 @@ begin
 			end
 			
 			else if(cnt == INPUT_DATA_WIDTH + 1) begin  // UART stop bit transmission
+				assert((state-NUMBER_OF_RX_SYNCHRONIZERS) == Rx_PARITY_BIT);
 				assert(shift_reg == 1);  // stop bit
 				assert(o_busy == 1);
 			end
@@ -206,7 +208,7 @@ begin
 			else if(cnt == (NUMBER_OF_BITS - 1)) begin // end of UART transmission
 				had_been_enabled <= 0;
 			
-				assert(state < Rx_STOP_BIT);
+				assert((state-NUMBER_OF_RX_SYNCHRONIZERS) == Rx_STOP_BIT);
 				assert(data_is_valid == 0);
 				assert(shift_reg == 0);
 				assert(serial_out == 1);   // default idle bit
